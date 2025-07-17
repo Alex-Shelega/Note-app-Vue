841abe9b1e748fdd0357c5974e9823c3f18dd615
Alex-Shelega <aleqsandr2005@gmail.com>
2025-07-17 19:33:06 +0400

    First steps

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ data.number }}</h1>
+  <button @click="increment">Inrement</button>
+  <button @click="() => data.number = 0">Reset</button>
+  <Card :number="data.number" />
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+import Card from './components/Card.vue'
+
+  const data = reactive({})
+  data.number = 0
+
+  function increment(){
+    data.number++
+  }
+</script>
+
+<style scoped></style>
